b1585e407b6c7dc5316ba2dc67c50e70489d4caf
kimjonggeolj <kim.jonggeol.j@gmail.com>
2019-11-05 13:53:57 -0500

    Changed how variant page is rendered

--- varinfo.Page.R
+++ varinfo.Page.R
@@ -4,28 +4,7 @@ simpleCap <- function(x) {
          collapse = " ")
 }
 
-
-output$varBox <- renderUI({
-  boxPlus(
-    title = "Variant",
-    uiOutput("panel3"),
-    width = ifelse(
-      input$layout,
-      6,
-      12),
-    closable = F,
-    status = "primary"
-  )
-})
-
-# ===== TO DO: make separate observeEvent for a new input: "varClickSearch", which imports its own variantTable.global
-observeEvent(input$varClick, {
-  if (runfromPlotly) {
-    var <- plotlyVariantTable
-    runfromPlotly <<- F
-  } else {
-    var <- variantTable.global[variantTable.global$`HG19_ID` == input$varPageId]
-  }
+varPageFunc <- function(var = var) {
   colnames(var) <- c("Exome name (hg19)",
                      # "Exome name (hg38)",
                      "Region",
@@ -56,19 +35,20 @@ observeEvent(input$varClick, {
                      # "Reseq-MAF (control)",
                      # "Reseq-number of participants (controls)",
                      # gnomad (hg19) + UKBB (hg38) Columns:
-                     "gnomAD Genome Allele Frequency (AF)",
-                     "gnomAD Popmax Filtering AF",
-                     "gnomAD Popmax Filtering AF-Controls",
-                     "gnomAD AF-male",
-                     "gnomAD AF-female",
-                     "gnomAD AF-African",
-                     "gnomAD AF-South Asian",
-                     "gnomAD AF-Latino",
-                     "gnomAD AF-East Asian",
-                     "gnomAD AF-European (non-Finnish)",
-                     "gnomAD AF-Finnish",
-                     "gnomAD AF-Ashkenazi Jewish",
-                     "gnomAD AF-Other")
+                     "Genome Allele Frequency (AF)", #gnomAD====================
+                     "Popmax Filtering AF",
+                     "Popmax Filtering AF-Controls",
+                     "AF-male",
+                     "AF-female",
+                     "AF-African",
+                     "AF-South Asian",
+                     "AF-Latino",
+                     "AF-East Asian",
+                     "AF-European (non-Finnish)",
+                     "AF-Finnish",
+                     "AF-Ashkenazi Jewish",
+                     "AF-Other"                     #gnomAD ends here===========
+  )
   # output$genome.freq.Table <- renderTable(
   #   var[, c(9:14)],
   #   digits = -2
@@ -76,8 +56,14 @@ observeEvent(input$varClick, {
   
   output$exome.freq.Table <- renderTable(
     {
-      table <- dcast(melt(var[,c(8:14)], id.vars = "Clinical significance (ClinVar)"), variable ~ `Clinical significance (ClinVar)`)
-      colnames(table) <- c("Population", "Frequency")
+      table.case <- dcast(melt(var[,c(8,9,11,12)], id.vars = "Clinical significance (ClinVar)"), variable ~ `Clinical significance (ClinVar)`)
+      colnames(table.case) <- c("Population", "Case")
+      table.case$Population <- gsub(' \\(\\w+\\)', '', table.case$Population)
+      table.control <- dcast(melt(var[,c(8,10,13,14)], id.vars = "Clinical significance (ClinVar)"), variable ~ `Clinical significance (ClinVar)`)
+      colnames(table.control) <- c("Population", "Control")
+      table.control$Population <- gsub(' \\(\\w+\\)', '', table.control$Population)
+      table <- merge(table.case, table.control, by = 'Population')
+      colnames(table)[1] <- ""
       table
     },
     digits = -2
@@ -92,6 +78,11 @@ observeEvent(input$varClick, {
     {
       table <- dcast(melt(var[, c(14:27)], id.vars = "Exome-number of participants (control)"), variable ~ `Exome-number of participants (control)`)
       colnames(table) <- c("Population", "Frequency")
+      table$Population <- gsub('gnomAD ', '', table$Population)
+      table$Population <- gsub(' Allele Frequency \\(AF\\)', '', table$Population)
+      table$Population <- gsub(' AF', '', table$Population)
+      table$Population <- gsub('AF-', '', table$Population)
+      #table$Population <- gsub('Other', 'Other Populations', table$Population)
       table
     },
     digits = -2
@@ -112,8 +103,19 @@ observeEvent(input$varClick, {
       ),
       column(width = 12,
              h2(var$rsID,
-                tags$i(paste0("(", toupper(var$`Nearest Gene`[1]#input$resPageId
-                ), ")")),
+                tags$i(
+                  HTML(
+                    paste0("(",
+                           a(
+                             onClick = "resClick(this.id)",
+                             href = "javascript:;",
+                             id = var$`Nearest Gene`[1],
+                             toupper(var$`Nearest Gene`[1]#input$resPageId
+                             )
+                           ),
+                           ")")
+                  )
+                ),
                 tags$sup(
                   a(href = paste0(
                     "https://www.ai-omni.com/search=",
@@ -138,48 +140,156 @@ observeEvent(input$varClick, {
                   a(href = paste0("https://www.ncbi.nlm.nih.gov/snp/", var$rsID[1]),
                     target = "_blank",
                     ifelse(var$rsID == "", "", "dbSNP"))
-                )),
-             div(tags$b("Region:"), simpleCap(var$Region[1]), style = "margin-bottom:20px;"),
-             div(tags$b("Amino Acid Change:"),
-                 gsub(".*(p\\..*)", "\\1", var$`Amino acid change`),
-                 style = "margin-bottom:2px;"),
-             div(tags$b("Functional Consequence:"),
-                 gsub("_", " ", var$`Functional consequence`),
-                 style = "margin-bottom:20px;"),
-             ifelse(var$`Conditions (ClinVar)` == ".", "", 
-                    tagList(div(tags$b("ClinVar (Conditions):"), var$`Conditions (ClinVar)`))),
-             ifelse(var$`Clinical significance (ClinVar)` == ".", "",
-                    tagList(div(tags$b("ClinVar (Clinical Significance): "), var$`Clinical significance (ClinVar)`,
-                                style = "margin-bottom:50px;"))),
+                ))#,
+             # div(tags$b("Region:"), simpleCap(var$Region[1]), style = "margin-bottom:20px;"),
+             # div(tags$b("Amino Acid Change:"),
+             #     gsub(".*(p\\..*)", "\\1", var$`Amino acid change`),
+             #     style = "margin-bottom:2px;"),
+             # div(tags$b("Functional Consequence:"),
+             #     gsub("_", " ", var$`Functional consequence`),
+             #     style = "margin-bottom:20px;"),
+             # ifelse(var$`Conditions (ClinVar)` == ".", "", 
+             #        tagList(div(tags$b("ClinVar (Conditions):"), var$`Conditions (ClinVar)`))),
+             # ifelse(var$`Clinical significance (ClinVar)` == ".", "",
+             #        tagList(div(tags$b("ClinVar (Clinical Significance): "), var$`Clinical significance (ClinVar)`,
+             #                    style = "margin-bottom:50px;")))#,
              
-             h3("Frequency Table")
+             #h2("Frequency Table")
+      )
+    ),
+    fluidRow(
+      column(
+        width = 4,
+            descriptionBlock(
+              header = "REGION",
+              text = simpleCap(var$Region[1]),
+              right_border = T,
+              margin_bottom = F
+            )
+        ),
+      column(
+        width = 4,
+            descriptionBlock(
+              header = "AMINO ACID CHANGE",
+              text = gsub(".*(p\\..*)", "\\1", var$`Amino acid change`),
+              right_border = T,
+              margin_bottom = F
+            )
+      ),
+      column(
+        width = 4,
+            descriptionBlock(
+              header = "FUNCTIONAL CONSEQUENCE",
+              text = gsub("_", " ", var$`Functional consequence`),
+              right_border = FALSE,
+              margin_bottom = F
+            )
+      ),
+    ),
+    fluidRow(
+      column(
+        width = 6,
+        descriptionBlock(
+          header = "ClinVar (Conditions)",
+          text = ifelse(
+            var$`Conditions (ClinVar)` == ".",
+            "N/A",
+            gsub(
+              ",",
+              ", ",
+              gsub(
+                "\\|",
+                " | ",
+                var$`Conditions (ClinVar)`
+              )
+            )
+            ),
+          right_border = T,
+          margin_bottom = F
+        )
+      ),
+      column(
+        width = 6,
+        descriptionBlock(
+          header = "ClinVar (Clinical Significance)",
+          text = ifelse(
+            var$`Conditions (ClinVar)` == ".",
+            "N/A",
+            gsub(
+              ",",
+              ", ",
+              gsub(
+                "\\|",
+                " | ",
+                var$`Clinical significance (ClinVar)`
+              )
+            )
+          ),
+          right_border = FALSE,
+          margin_bottom = F
+        )
       )
     ),
     fluidRow(
       column(
         width = 6,
-        h4("IPDGC"),
+        h3("IPDGC"),
         # h5("Genome"),
         # div(
         #   tableOutput("genome.freq.Table")
         # ),
-        h5("Exome"),
+        h4("Exome"),
         div(
           tableOutput("exome.freq.Table")
-        )#,
-        # h5("Reseq"),
-        # div(
-        #   tableOutput("reseq.freq.Table ")
-        # )
+        ),
       ),
       column(
         width = 6,
-        h4("Other Resources"),
+        h3("Other Resources"),
+        h4(a("gnomAD", href = "https://gnomad.broadinstitute.org/", target = "_blank")),
         div(
           tableOutput("others.freq.Table")
         )
-      )
-    )#,
+      )#,
+      # column(
+      #   width = 6,
+      #   boxPad(
+      #     color = "blue",
+      #     descriptionBlock(
+      #       header = "REGION", 
+      #       text = simpleCap(var$Region[1]), 
+      #       right_border = FALSE,
+      #       margin_bottom = TRUE
+      #     ),
+      #     descriptionBlock(
+      #       header = "AMINO ACID CHANGE", 
+      #       text = gsub(".*(p\\..*)", "\\1", var$`Amino acid change`), 
+      #       right_border = FALSE,
+      #       margin_bottom = TRUE
+      #     ),
+      #     descriptionBlock(
+      #       header = "FUNCTIONAL CONSEQUENCE", 
+      #       text = gsub("_", " ", var$`Functional consequence`), 
+      #       right_border = FALSE,
+      #       margin_bottom = FALSE
+      #     )
+      #   ),
+      #   boxPad(
+      #     color = "teal",
+      #     descriptionBlock(
+      #       header = "ClinVar (Conditions)", 
+      #       text = ifelse(var$`Conditions (ClinVar)` == ".", "N/A", var$`Conditions (ClinVar)`), 
+      #       right_border = FALSE,
+      #       margin_bottom = TRUE
+      #     ),
+      #     descriptionBlock(
+      #       header = "ClinVar (Clinical Significance)", 
+      #       text = ifelse(var$`Clinical significance (ClinVar)` == ".", "N/A", var$`Clinical significance (ClinVar)`), 
+      #       right_border = FALSE,
+      #       margin_bottom = TRUE
+      #     ),
+      #   )
+      # )#,
     # hr(),
     # fluidRow(
     #   column(
@@ -192,6 +302,32 @@ observeEvent(input$varClick, {
     # )
   )
   )
+  )
+}
+
+
+output$varBox <- renderUI({
+  boxPlus(
+    title = "Variant",
+    uiOutput("panel3"),
+    width = ifelse(
+      input$layout,
+      6,
+      12),
+    closable = F,
+    status = "primary"
+  )
+})
+
+# ===== TO DO: make separate observeEvent for a new input: "varClickSearch", which imports its own variantTable.global
+observeEvent(input$varClick, {
+  if (runfromPlotly) {
+    var <- plotlyVariantTable
+    runfromPlotly <<- F
+  } else {
+    var <- variantTable.global[variantTable.global$`HG19_ID` == input$varPageId]
+  }
+  varPageFunc(var)
   #shinyjs::show("varBox")
 })
 
@@ -252,183 +388,7 @@ observeEvent(input$varResClick, {
                                                       "AF_asj",
                                                       "AF_oth"
   )]
+  varPageFunc(var)
   # var <- variantTable.global[variantTable.global$`HG19_ID` == input$varPageId]
-  colnames(var) <- c("Exome name (hg19)",
-                     # "Exome name (hg38)",
-                     "Region",
-                     "Functional consequence",
-                     "Amino acid change",
-                     "Nearest Gene",
-                     "rsID",
-                     "Conditions (ClinVar)",
-                     "Clinical significance (ClinVar)",
-                     # IPDGC Genomes (hg38) Columns:
-                     # "Genomes-Distribution (cases)",
-                     # "Genomes-Distribution (control)",
-                     # "Genomes-MAF (cases)",
-                     # "Genomes-number of participants (cases)",
-                     # "Genomes-MAF (control)",
-                     # "Genomes-number of participants (control)",
-                     # IPDGC Exomes + ReSeq (hg19) Columns:
-                     "Exome-Distribution (cases)",
-                     "Exome-Distribution (control)",
-                     "Exome-MAF (cases)",
-                     "Exome-number of participants (cases)",
-                     "Exome-MAF (control)",
-                     "Exome-number of participants (control)",
-                     # "Reseq-Distribution (cases)",
-                     # "Reseq-Distribution (controls)",
-                     # "Reseq-MAF (case)",
-                     # "Reseq-number of participants (cases)",
-                     # "Reseq-MAF (control)",
-                     # "Reseq-number of participants (controls)",
-                     # gnomad (hg19) + UKBB (hg38) Columns:
-                     "Genome Allele Frequency (AF)", #gnomAD====================
-                     "Popmax Filtering AF",
-                     "Popmax Filtering AF-Controls",
-                     "AF-male",
-                     "AF-female",
-                     "AF-African",
-                     "AF-South Asian",
-                     "AF-Latino",
-                     "AF-East Asian",
-                     "AF-European (non-Finnish)",
-                     "AF-Finnish",
-                     "AF-Ashkenazi Jewish",
-                     "AF-Other"                     #gnomAD ends here===========
-                     )
-  # output$genome.freq.Table <- renderTable(
-  #   var[, c(9:14)],
-  #   digits = -2
-  # )
-  
-  output$exome.freq.Table <- renderTable({
-    table <- dcast(melt(var[,c(8:14)], id.vars = "Clinical significance (ClinVar)"), variable ~ `Clinical significance (ClinVar)`)
-    colnames(table) <- c("Population", "Frequency")
-    table
-  },
-  digits = -2
-  )
-  
-  # output$reseq.freq.Table <- renderTable(
-  #   var[,c(21:26)],
-  #   digits = -2
-  # )
-  
-  output$others.freq.Table <- renderTable({
-    table <- dcast(melt(var[, c(14:27)], id.vars = "Exome-number of participants (control)"), variable ~ `Exome-number of participants (control)`)
-    colnames(table) <- c("Population", "Frequency")
-    table
-  },
-  digits = -2
-  )
-  
-  var$rsID <- ifelse(var$rsID == ".", "", var$rsID)
-  output$panel3 <- renderUI(tagList(
-    fluidRow(
-      column(width = 12,
-             h1(
-               tags$b("Variant:"),
-               var$`Exome name (hg19)`,
-               "(hg19)")#,
-             # h1(HTML("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"),
-             #    var$`Exome name (hg38)`,
-             #    "(hg38)"
-             #    )
-      ),
-      column(width = 12,
-             h2(var$rsID,
-                tags$i(
-                  HTML(
-                    paste0("(",
-                           a(
-                             onClick = "resClick(this.id)",
-                             href = "javascript:;",
-                             id = var$`Nearest Gene`[1],
-                             toupper(var$`Nearest Gene`[1]#input$resPageId
-                             )
-                           ),
-                           ")")
-                    )
-                  ),
-                tags$sup(
-                  a(href = paste0(
-                    "https://www.ai-omni.com/search=",
-                    gsub("(\\d+:\\d+).+", "chr\\1", var[1,1]),
-                    "/page=1"),
-                    target = "_blank",
-                    "omni"),
-                  a(href = paste0("https://gnomad.broadinstitute.org/variant/", var[1,1]),
-                    target = "_blank",
-                    "gnomad"),
-                  a(href = paste0("https://bravo.sph.umich.edu/freeze3a/hg19/variant/",
-                                  gsub("(\\d+):(\\d+):(.*):(.*)", "\\1-\\2-\\3-\\4", var[1,1])
-                  ),
-                  target = "_blank",
-                  "bravo"),
-                  # BRAVO for hg38 freeze5
-                  # a(href = paste0("https://bravo.sph.umich.edu/freeze5/hg38/variant/",
-                  #                 gsub("(\\d+):(\\d+):(.*):(.*)", "\\1-\\2-\\3-\\4", var[1,2])
-                  #                      ),
-                  #   target = "_blank",
-                  #   "bravo"),
-                  a(href = paste0("https://www.ncbi.nlm.nih.gov/snp/", var$rsID[1]),
-                    target = "_blank",
-                    ifelse(var$rsID == "", "", "dbSNP"))
-                )),
-             div(tags$b("Region:"), simpleCap(var$Region[1]), style = "margin-bottom:20px;"),
-             div(tags$b("Amino Acid Change:"),
-                 gsub(".*(p\\..*)", "\\1", var$`Amino acid change`),
-                 style = "margin-bottom:2px;"),
-             div(tags$b("Functional Consequence:"),
-                 gsub("_", " ", var$`Functional consequence`),
-                 style = "margin-bottom:20px;"),
-             ifelse(var$`Conditions (ClinVar)` == ".", "", 
-                    tagList(div(tags$b("ClinVar (Conditions):"), var$`Conditions (ClinVar)`))),
-             ifelse(var$`Clinical significance (ClinVar)` == ".", "",
-                    tagList(div(tags$b("ClinVar (Clinical Significance): "), var$`Clinical significance (ClinVar)`,
-                                style = "margin-bottom:50px;"))),
-             
-             h2("Frequency Table")
-      )
-    ),
-    fluidRow(
-      column(
-        width = 6,
-        h3("IPDGC"),
-        # h5("Genome"),
-        # div(
-        #   tableOutput("genome.freq.Table")
-        # ),
-        h4("Exome"),
-        div(
-          tableOutput("exome.freq.Table")
-        )#,
-        # h5("Reseq"),
-        # div(
-        #   tableOutput("reseq.freq.Table ")
-        # )
-      ),
-      column(
-        width = 6,
-        h3("Other Resources"),
-        h4(a("gnomAD", href = "https://gnomad.broadinstitute.org/", target = "_blank")),
-        div(
-          tableOutput("others.freq.Table")
-        )
-      )
-    )#,
-    # hr(),
-    # fluidRow(
-    #   column(
-    #     width = 12,
-    #     h4("Other Resources"),
-    #     div(
-    #       tableOutput("others.freq.Table")
-    #     )
-    #   )
-    # )
-  )
-  )
   #shinyjs::show("varBox")
 })
